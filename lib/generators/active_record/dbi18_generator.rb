2bbdd95ecb4a01a471e08e0b16a010c1bb2c92e0
pockycat <gaogao1130@gmail.com>
2013-09-29 16:56:31 +0800

    first post

--- lib/generators/active_record/dbi18_generator.rb
+++ lib/generators/active_record/dbi18_generator.rb
@@ -7,14 +7,15 @@ module ActiveRecord
       source_root File.expand_path("../templates", __FILE__)
       argument :dbi18_class, type: :string, banner: "dbi18", desc: "dbi18", default: "dbi18"
 
-      # def show
-      #   p "#{table_name}"
-      #   p "#{class_name}"
-      #   p "#{name}"
-      # end
+       def show
+          p "#{table_name}".singularize
+          p "#{table_name}"
+          p "#{class_name}"
+          p "#{name}"
+       end
 
       def generate_auth_item_model
-         invoke "active_record:model", ["#{name}", "class_id", "class_name", "language_type", "hash_content",   "--no-migration"], :migration => false
+         invoke "active_record:model", ["#{name}", "classtable_id", "classtable_type", "language_type", "hash_content",   "--no-migration"], :migration => false
         # Rails::Generators.invoke("active_record:model", ["db_i18", "class_id", "class_name", "property", "hash_content",  "--no-migration"], behavior: behavior)
       end
 
@@ -22,7 +23,15 @@ module ActiveRecord
          migration_template "migration.rb", "db/migrate/create_" + "#{table_name}".singularize
       end
 
+      def inject_user_content
+        inject_into_file(model_path, "  belongs_to :classtable, :polymorphic => true\n", :after => inject_dbi18_method)  if behavior == :invoke
+      end
+
       protected
+
+      def inject_dbi18_method
+        /class[ |\t]+#{class_name}\n|class[ |\t]+#{class_name}.*\n|class[ |\t]+#{class_name.demodulize}\n|class[ |\t]+#{class_name.demodulize}.*\n/
+      end
 
       def model_path
          File.join("app", "models", "#{table_name}".singularize + ".rb")
--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,15 +1,15 @@
 class Create<%= class_name %>< ActiveRecord::Migration
   def change
     create_table(:<%= table_name %>) do |t|
-      t.integer :class_id
-      t.string  :class_name
+      t.integer :classtable_id
+      t.string  :classtable_type
       t.string  :language_type
       t.text    :hash_content
       t.timestamps
     end
 
-    add_index(:<%= table_name %>, :class_id)
-    add_index(:<%= table_name %>, :class_name)
+    add_index(:<%= table_name %>, :classtable_id)
+    add_index(:<%= table_name %>, :classtable_type)
     add_index(:<%= table_name %>, :language_type)
     add_index(:<%= table_name %>, :hash_content)
   end
