db7d2302f7f6b2d1fb31e444bb579499f08c7d94
pockycat <gaogao1130@gmail.com>
2013-09-30 14:53:28 +0800

    fix initializer

--- lib/generators/dbi18/templates/initializer.rb
+++ lib/generators/dbi18/templates/initializer.rb
@@ -1,6 +1,6 @@
 Dbi18.configure do |config|
 	# For the time being, maybe in the long term, it only support ActiveRecord...
 	# models and database tables
-	# config.language_type = [:en,:zh]
+	# config.locale = [:en,:zh]
 	config.model = <%= class_name %>
 end
